a3bdd0b23273c63c2288f725ad6915b87e25faf3
MikamiYudai <241122@std.hi-joho.ac.jp>
2025-05-16 12:30:17 +0900

    課題4作成

---
+++ src/Review4.java
@@ -0,0 +1,14 @@
+public class Review4 {
+    public static void main(String[] args) {
+        int number = 5;
+        String ste = 88 + "88";
+        final double PI = 3.141592;
+        double PA= 3.14;
+        float circ = 3.14f;
+        long a =10000000000L;
+        boolean flg = true;
+        byte b =35;
+        int num = 0x735;
+        char c = 'a';
+    }
+}
