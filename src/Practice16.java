46496e1bc776ad6353deb66de4e786f361eec04e
MikamiYudai <241122@std.hi-joho.ac.jp>
2025-05-19 12:13:29 +0900

    練習問題16作成

---
+++ src/Practice16.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class Practice16 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int sum = 0;
+        int i = 1;
+        while (i <= num) {
+            sum += i;
+            i++;
+        }
+        System.out.println("結果："+sum);
+    }
+}
