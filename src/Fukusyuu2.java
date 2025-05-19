e2bbbc4b618b13f3bc63512ada86c6a9ca736251
MikamiYudai <241122@std.hi-joho.ac.jp>
2025-05-19 11:30:12 +0900

    復習問題2作成

---
+++ src/Fukusyuu2.java
@@ -0,0 +1,9 @@
+public class Fukusyuu2 {
+    public static void main(String[] args) {
+        int num1;
+        double num2;
+        num1 = 50;
+        num2 =20.0;
+        System.out.println(num1*num2);
+    }
+}
