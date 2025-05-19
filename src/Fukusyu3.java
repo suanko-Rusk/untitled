6639cf44e0fd071a0f938e2bfc858d8cc35edf3a
MikamiYudai <241122@std.hi-joho.ac.jp>
2025-05-19 11:27:16 +0900

    復習問題１，３作成

---
+++ src/Fukusyu3.java
@@ -0,0 +1,9 @@
+public class Fukusyu3 {
+    public static void main(String[] args) {
+        String str1;
+        String chr1;
+        str1 = "iphon";
+        chr1 = "X";
+        System.out.println(str1+chr1);
+    }
+}
---
+++ src/Fukusyuu1.java
@@ -0,0 +1,9 @@
+public class Fukusyuu1 {
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+        num1 = 10;
+        num2 = 20;
+        System.out.println(num1 + num2);
+    }
+}
