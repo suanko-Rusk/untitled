ae9ba1ff864286b29c932a611926443604da5ae5
MikamiYudai <241122@std.hi-joho.ac.jp>
2025-05-19 12:05:50 +0900

    練習問題１５作成

---
+++ src/practice15.java
@@ -0,0 +1,12 @@
+public class practice15 {
+    public static void main(String[] args) {
+        int num = 12;
+        if (num % 2 == 0) {
+            System.out.println("偶数");
+        } else if (num % 2 !=0) {
+            System.out.println("奇数");
+        } else {
+            System.out.println("無効な入力です。");
+        }
+    }
+}
