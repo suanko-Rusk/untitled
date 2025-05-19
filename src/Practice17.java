e512287dced78593e42b2d37b299db40527aa1cb
MikamiYudai <241122@std.hi-joho.ac.jp>
2025-05-19 12:25:37 +0900

    練習問題６作成

---
+++ src/Practice17.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class Practice17 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int sum = 0;
+        int i = 2;
+        while (i <= num) {
+            sum += i;
+            i += 2;
+        }
+        System.out.println("結果：" + sum);
+    }
+}
