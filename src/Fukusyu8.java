20c88e0d496d8227fa3d03bc76e50c01b4cdf23b
MikamiYudai <241122@std.hi-joho.ac.jp>
2025-05-26 11:57:32 +0900

    練習問題20作成 復習問題7,8作成

---
+++ src/Fukusyu8.java
@@ -0,0 +1,16 @@
+public class Fukusyu8 {
+    public static void main(String[] args) {
+        int[] t = {3,5,7,2,8,6};
+        int max = 0;
+        int min = 100;
+        for (int i = 0; i < t.length; i++) {
+            if (max < t[i]) {
+                max = t[i];
+            }
+            if (min > t[i]) {
+                min = t[i];
+            }
+        }
+        System.out.println("最大値"+max+ ",最小値"+min);
+    }
+}
