ae2d92ec3d5adf26b800d344200bf8bc6fbe0bf2
MikamiYudai <241122@std.hi-joho.ac.jp>
2025-06-13 12:05:51 +0900

    Review23作成

---
+++ src/Review/Review23.java
@@ -0,0 +1,12 @@
+package Review;
+
+public class Review23 {
+    public static void main(String[] args) {
+        int num = 6;
+        int sum = 1;
+        for (int i = 1; i <= num; i++) {
+            sum *= i;
+        }
+        System.out.println("結果:" + sum);
+    }
+}
