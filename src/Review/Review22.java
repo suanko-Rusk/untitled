93f4998c2859c20141c3a27ec4b5cf1f1f621ed2
MikamiYudai <241122@std.hi-joho.ac.jp>
2025-06-13 12:01:28 +0900

    Review22作成

---
+++ src/Review/Review22.java
@@ -0,0 +1,12 @@
+package Review;
+
+public class Review22 {
+    public static void main(String[] args) {
+        int num = 8;
+        int sum = 0;
+        for (int i = 1; i <= num; i+=2) {
+            sum += i;
+        }
+        System.out.println("結果:" + sum);
+    }
+}
